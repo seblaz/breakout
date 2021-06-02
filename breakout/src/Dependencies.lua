06896a0e81b1dacee8d52f5fbef490978ae23b43
seblaz <sebastian.blazquez96@gmail.com>
2021-06-01 23:02:16 -0300

    Moved files to model folder

--- breakout/src/Dependencies.lua
+++ breakout/src/Dependencies.lua
@@ -16,24 +16,24 @@ Class = require 'lib/class'
 require 'src/constants'
 
 -- the ball that travels around, breaking bricks and triggering lives lost
-require 'src/Ball'
+require 'src/model/Ball'
 
 -- the entities in our game map that give us points when we collide with them
-require 'src/Brick'
+require 'src/model/Brick'
 
 -- a class used to generate our brick layouts (levels)
-require 'src/LevelMaker'
+require 'src/model/LevelMaker'
 
 -- the rectangular entity the player controls, which deflects the ball
-require 'src/Paddle'
+require 'src/model/Paddle'
 
 -- a basic StateMachine class which will allow us to transition to and from
 -- game states smoothly and avoid monolithic code in one file
-require 'src/StateMachine'
+require 'src/model/StateMachine'
 
 -- utility functions, mainly for splitting our sprite sheet into various Quads
 -- of differing sizes for paddles, balls, bricks, etc.
-require 'src/Util'
+require 'src/model/Util'
 
 -- each of the individual states our game can be in at once; each state has
 -- its own update and render methods that can be called by our state machine
