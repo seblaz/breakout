eab411d8812dd0c0354bfd131ccef3166b4fa1be
seblaz <sebastian.blazquez96@gmail.com>
2021-06-06 20:33:17 -0300

    Move high score load

--- breakout/main.lua
+++ breakout/main.lua
@@ -27,6 +27,7 @@
 require 'src/Dependencies'
 
 local Score = require 'src/model/Score'
+local HighScoreRepo = require 'src/repositories/HighScore'
 
 --[[
     Called just once at the beginning of the game; used to set up
@@ -121,7 +122,7 @@ function love.load()
         ['paddle-select'] = function() return PaddleSelect() end
     }
     gStateMachine:change('start', {
-        highScores = loadHighScores()
+        highScores = HighScoreRepo():load()
     })
 
     -- play our music outside of all states and set it to looping
@@ -212,47 +213,6 @@ function love.draw()
     displayFPS()
     
     push:apply('end')
-end
-
---[[
-    Loads high scores from a .lst file, saved in LÖVE2D's default save directory in a subfolder
-    called 'breakout'.
-]]
-function loadHighScores()
-    love.filesystem.setIdentity('breakout')
-
-    -- if the file doesn't exist, initialize it with some default scores
-    if not love.filesystem.getInfo('breakout.lst') then
-        local scores = ''
-        for i = 10, 1, -1 do
-            scores = scores .. 'CTO\n'
-            scores = scores .. tostring(i * 1000) .. '\n'
-        end
-
-        love.filesystem.write('breakout.lst', scores)
-    end
-
-    -- flag for whether we're reading a name or not
-    local name = true
-    local counter = 1
-
-    -- initialize scores table with at least 10 blank entries
-    local scores = {}
-
-    -- iterate over each line in the file, filling in names and scores
-    for line in love.filesystem.lines('breakout.lst') do
-        if name then
-            scores[counter] = Score(0, string.sub(line, 1, 3))
-        else
-            scores[counter]:add(tonumber(line))
-            counter = counter + 1
-        end
-
-        -- flip the name flag
-        name = not name
-    end
-
-    return scores
 end
 
 --[[
---
+++ breakout/src/repositories/HighScore.lua
@@ -0,0 +1,54 @@
+local Object = require 'src/Object'
+local Score = require 'src/model/Score'
+
+local HighScore = Object()
+
+function HighScore:save()
+    local scoresStr = ''
+
+    for i = 1, 10 do
+        scoresStr = scoresStr .. self.highScores[i]:name() .. '\n'
+        scoresStr = scoresStr .. tostring(self.highScores[i]:points()) .. '\n'
+    end
+
+    love.filesystem.write('breakout.lst', scoresStr)
+end
+
+function HighScore:load()
+    love.filesystem.setIdentity('breakout')
+
+    -- if the file doesn't exist, initialize it with some default scores
+    if not love.filesystem.getInfo('breakout.lst') then
+        local scores = ''
+        for i = 10, 1, -1 do
+            scores = scores .. 'CTO\n'
+            scores = scores .. tostring(i * 1000) .. '\n'
+        end
+
+        love.filesystem.write('breakout.lst', scores)
+    end
+
+    -- flag for whether we're reading a name or not
+    local name = true
+    local counter = 1
+
+    -- initialize scores table with at least 10 blank entries
+    local scores = {}
+
+    -- iterate over each line in the file, filling in names and scores
+    for line in love.filesystem.lines('breakout.lst') do
+        if name then
+            scores[counter] = Score(0, string.sub(line, 1, 3))
+        else
+            scores[counter]:add(tonumber(line))
+            counter = counter + 1
+        end
+
+        -- flip the name flag
+        name = not name
+    end
+
+    return scores
+end
+
+return HighScore
